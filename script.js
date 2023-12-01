8d7377990fb54a71184cfcf187c4fcfa8611c575
mrunal3006 <128721188+mrunal3006@users.noreply.github.com>
2023-12-01 13:08:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function () {
+    // Get the counter element and increment button
+    var counterElement = document.getElementById("counter");
+    var incrementBtn = document.getElementById("incrementBtn");
+
+    // Set initial counter value
+    var counterValue = 0;
+    counterElement.textContent = counterValue;
+
+    // Add click event listener to the increment button
+    incrementBtn.addEventListener("click", function () {
+        // Display alert with the current counter value before incrementing
+        alert("Un-incremented value: " + counterValue);
+
+        // Increment the counter value
+        counterValue++;
+
+        // Update the counter element with the new value
+        counterElement.textContent = counterValue;
+    });
+});
